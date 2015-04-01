872eb601c5384579c0ec647b5c08983effcee37b
Juan Gomez <erioly@gmail.com>
2015-04-01 00:47:41 -0400

    Complete styling for login page

--- app/build/generated/source/r/debug/com/teamtreehouse/ribbit/R.java
+++ app/build/generated/source/r/debug/com/teamtreehouse/ribbit/R.java
@@ -14,48 +14,73 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int background_purple=0x7f050000;
+        public static final int apptheme_color=0x7f050000;
+        public static final int background_purple=0x7f050001;
+        public static final int focused_purple_button=0x7f050002;
+        public static final int light_gray=0x7f050003;
+        public static final int light_purple_button=0x7f050004;
+        public static final int pressed_purple_button=0x7f050005;
+        public static final int text_color=0x7f050006;
     }
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f060000;
         public static final int activity_vertical_margin=0x7f060001;
-        public static final int message_list_item_vertical_margin=0x7f060002;
+        public static final int login_horizontal_padding=0x7f060002;
+        public static final int login_vertical_margin=0x7f060003;
+        public static final int login_vertical_padding=0x7f060004;
+        public static final int message_list_item_vertical_margin=0x7f060005;
     }
     public static final class drawable {
-        public static final int avatar_empty=0x7f020000;
-        public static final int avatar_selected=0x7f020001;
-        public static final int background=0x7f020002;
-        public static final int background_fill=0x7f020003;
-        public static final int ic_action_camera=0x7f020004;
-        public static final int ic_action_picture=0x7f020005;
-        public static final int ic_action_play_over_video=0x7f020006;
-        public static final int ic_action_send_now=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int ic_menu_camera=0x7f020009;
-        public static final int ic_menu_overflow=0x7f02000a;
-        public static final int ic_menu_send=0x7f02000b;
-        public static final int ic_picture=0x7f02000c;
-        public static final int ic_tab_friends=0x7f02000d;
-        public static final int ic_tab_inbox=0x7f02000e;
-        public static final int ic_video=0x7f02000f;
+        public static final int apptheme_btn_default_disabled_focused_holo_light=0x7f020000;
+        public static final int apptheme_btn_default_disabled_holo_light=0x7f020001;
+        public static final int apptheme_btn_default_focused_holo_light=0x7f020002;
+        public static final int apptheme_btn_default_holo_light=0x7f020003;
+        public static final int apptheme_btn_default_normal_holo_light=0x7f020004;
+        public static final int apptheme_btn_default_pressed_holo_light=0x7f020005;
+        public static final int apptheme_edit_text_holo_light=0x7f020006;
+        public static final int apptheme_textfield_activated_holo_light=0x7f020007;
+        public static final int apptheme_textfield_default_holo_light=0x7f020008;
+        public static final int apptheme_textfield_disabled_focused_holo_light=0x7f020009;
+        public static final int apptheme_textfield_disabled_holo_light=0x7f02000a;
+        public static final int apptheme_textfield_focused_holo_light=0x7f02000b;
+        public static final int avatar_empty=0x7f02000c;
+        public static final int avatar_selected=0x7f02000d;
+        public static final int background=0x7f02000e;
+        public static final int background_fill=0x7f02000f;
+        public static final int button_custom=0x7f020010;
+        public static final int ic_action_camera=0x7f020011;
+        public static final int ic_action_picture=0x7f020012;
+        public static final int ic_action_play_over_video=0x7f020013;
+        public static final int ic_action_send_now=0x7f020014;
+        public static final int ic_launcher=0x7f020015;
+        public static final int ic_menu_camera=0x7f020016;
+        public static final int ic_menu_overflow=0x7f020017;
+        public static final int ic_menu_send=0x7f020018;
+        public static final int ic_picture=0x7f020019;
+        public static final int ic_tab_friends=0x7f02001a;
+        public static final int ic_tab_inbox=0x7f02001b;
+        public static final int ic_video=0x7f02001c;
     }
     public static final class id {
-        public static final int action_camera=0x7f0a000e;
-        public static final int action_edit_friends=0x7f0a000c;
-        public static final int action_logout=0x7f0a000d;
-        public static final int action_send=0x7f0a000f;
+        public static final int action_camera=0x7f0a0011;
+        public static final int action_edit_friends=0x7f0a000f;
+        public static final int action_logout=0x7f0a0010;
+        public static final int action_send=0x7f0a0012;
         public static final int backgroundImage=0x7f0a0000;
-        public static final int emailField=0x7f0a0006;
-        public static final int imageView=0x7f0a0008;
-        public static final int loginButton=0x7f0a0003;
-        public static final int messageIcon=0x7f0a000a;
-        public static final int pager=0x7f0a0005;
-        public static final int passwordField=0x7f0a0002;
-        public static final int section_label=0x7f0a0009;
-        public static final int senderLabel=0x7f0a000b;
-        public static final int signUpText=0x7f0a0004;
-        public static final int signupButton=0x7f0a0007;
-        public static final int usernameField=0x7f0a0001;
+        public static final int editTextLayout=0x7f0a0003;
+        public static final int emailField=0x7f0a0009;
+        public static final int imageView=0x7f0a000b;
+        public static final int loginButton=0x7f0a0006;
+        public static final int messageIcon=0x7f0a000d;
+        public static final int pager=0x7f0a0008;
+        public static final int passwordField=0x7f0a0005;
+        public static final int section_label=0x7f0a000c;
+        public static final int senderLabel=0x7f0a000e;
+        public static final int signUpText=0x7f0a0007;
+        public static final int signupButton=0x7f0a000a;
+        public static final int subtitle=0x7f0a0002;
+        public static final int title=0x7f0a0001;
+        public static final int usernameField=0x7f0a0004;
     }
     public static final class layout {
         public static final int activity_edit_friends=0x7f030000;
@@ -103,30 +128,26 @@ public final class R {
         public static final int sign_up_text=0x7f07001a;
         public static final int signup_error_message=0x7f07001b;
         public static final int signup_error_title=0x7f07001c;
-        public static final int success_message=0x7f07001d;
-        public static final int title_activity_edit_friends=0x7f07001e;
-        public static final int title_activity_login=0x7f07001f;
-        public static final int title_activity_recipients=0x7f070020;
-        public static final int title_activity_sign_up=0x7f070021;
-        public static final int title_activity_view_image=0x7f070022;
-        public static final int title_section1=0x7f070023;
-        public static final int title_section2=0x7f070024;
-        public static final int username_hint=0x7f070025;
-        public static final int video_file_size_warning=0x7f070026;
-        public static final int video_size_limit_warning=0x7f070027;
+        public static final int subtitle=0x7f07001d;
+        public static final int success_message=0x7f07001e;
+        public static final int title_activity_edit_friends=0x7f07001f;
+        public static final int title_activity_login=0x7f070020;
+        public static final int title_activity_recipients=0x7f070021;
+        public static final int title_activity_sign_up=0x7f070022;
+        public static final int title_activity_view_image=0x7f070023;
+        public static final int title_section1=0x7f070024;
+        public static final int title_section2=0x7f070025;
+        public static final int username_hint=0x7f070026;
+        public static final int video_file_size_warning=0x7f070027;
+        public static final int video_size_limit_warning=0x7f070028;
     }
     public static final class style {
-        /** 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
- API 11 theme customizations can go here. 
+        /**  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
-        /**  All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppBaseTheme=0x7f080003;
+        public static final int AppTheme=0x7f080000;
+        public static final int ImageButtonAppTheme=0x7f080001;
+        public static final int _AppTheme=0x7f080002;
     }
 }
