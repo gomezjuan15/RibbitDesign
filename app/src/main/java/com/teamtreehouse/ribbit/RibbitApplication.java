f5c35bd03bafc01bd3445531101cdb77280a6c07
Juan Gomez <erioly@gmail.com>
2015-04-09 00:46:41 -0400

    adding push notification

--- app/src/main/java/com/teamtreehouse/ribbit/RibbitApplication.java
+++ app/src/main/java/com/teamtreehouse/ribbit/RibbitApplication.java
@@ -3,12 +3,27 @@ package com.teamtreehouse.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.PushService;
+import com.teamtreehouse.ribbit.utils.ParseConstants;
 
 public class RibbitApplication extends Application {
 	
 	@Override
 	public void onCreate() { 
 		super.onCreate();
-        Parse.initialize(this, "FGgIIOAC6owh8g7PNGwyUMLzXYxLTu03yELovirM", "vGfh3itGgO6sfkfLRyXith5Fpns9OIg7AuBhOd3N");
+        Parse.initialize(this,
+                "FGgIIOAC6owh8g7PNGwyUMLzXYxLTu03yELovirM",
+                "vGfh3itGgO6sfkfLRyXith5Fpns9OIg7AuBhOd3N");
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 	}
+
+    public static void updateParseInstallation(ParseUser user) {
+
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put(ParseConstants.KEY_USER_ID, user.getObjectId());
+        installation.saveInBackground();
+    }
 }
--- app/src/main/java/com/teamtreehouse/ribbit/ui/RecipientsActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/ui/RecipientsActivity.java
@@ -25,7 +25,9 @@ import android.widget.Toast;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
@@ -203,6 +205,7 @@ public class RecipientsActivity extends Activity {
 				if (e == null) {
 					// success!
 					Toast.makeText(RecipientsActivity.this, R.string.success_message, Toast.LENGTH_LONG).show();
+                    sendPushNotifications();
 				}
 				else {
 					AlertDialog.Builder builder = new AlertDialog.Builder(RecipientsActivity.this);
@@ -215,6 +218,18 @@ public class RecipientsActivity extends Activity {
 			}
 		});
 	}
+
+    private void sendPushNotifications() {
+
+        ParseQuery<ParseInstallation> query = ParseInstallation.getQuery();
+        query.whereContainedIn(ParseConstants.KEY_USER_ID, getRecipientIds());
+
+        ParsePush push = new ParsePush();
+        push.setQuery(query);
+        push.setMessage(getString(R.string.push_message,
+                ParseUser.getCurrentUser().getUsername()));
+        push.sendInBackground();
+    }
 
     private AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
         @Override
